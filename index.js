e05a083a91287b9ea7f05307460f9d41edc1d9f2
joereger <joe@joereger.com>
2014-10-04 13:54:27 -0400

    tracking down repeating webhook call

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
 
 app.post('/test-webhook',function(req,res) {
 
-    //console.log(req);
+    console.log('Connection from: ' + req.connection.remoteAddress);
 
     var reply = slack.respond(req,function(hook) {
 
@@ -31,5 +31,5 @@ app.post('/test-webhook',function(req,res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at port:" + app.get('port'))
 })
