fece4dc5a9ff0d66a92f642ed2a5016080880a6c
joereger <joe@joereger.com>
2014-10-04 12:54:16 -0400

    first checkin, added node-slack dependency

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express();
+
+
+var Slack = require('node-slack');
+var slack = new Slack('springbot.slack.com','T2kiFFhCfuaYSfNgIteabbs8');
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello Superfly!')
+})
+
+app.post('/slack-webhook',function(req,res) {
+
+    var reply = slack.respond(req.body,function(hook) {
+
+        return {
+            text: 'Booyah, ' + hook.user_name,
+            username: 'Superfly'
+        };
+
+    });
+
+    res.json(reply);
+
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
