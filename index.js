0bac5665d5626e1aac1b3f7c63f03f796239e623
joereger <joe@joereger.com>
2014-10-05 10:55:46 -0400

    sorted out the repeating issue

--- index.js
+++ index.js
@@ -1,59 +1,46 @@
-var token = 'T2kiFFhCfuaYSfNgIteabbs8';
+
+//node-slack config... using two webhook integrations on Slack side
+var token_inbound_from_slack = 'T2kiFFhCfuaYSfNgIteabbs8';
+var token_outbound_to_slack = 'g6sDbTj7WLjD8cK2AcOTHS5h';
 var domain = 'springbot';
 var Slack = require('node-slack');
-var slack = new Slack(domain, token);
+var slack_in = new Slack(domain, token_inbound_from_slack);
+var slack_out = new Slack(domain, token_outbound_to_slack);
 
+//express
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
+//middleware body-parser
 var bodyParser = require('body-parser');
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded() );
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-
+//default path
 app.get('/', function(request, response) {
   response.send('Hello Superfly!')
 });
 
+//slack sending stuff to this app
 app.post('/test-webhook', function(request,response) {
 
-
-    if (request.body != null){
-        console.log('Request.body: ' + request.body);
-        console.log('Request.body.token: ' + request.body.token);
-        console.log('Request.body.user_name: ' + request.body.user_name);
-    } else {
-        console.log('Request.body: null');
-    }
-
-
-    if (request.query != null){
-        console.log('Request.query: ' + request.query);
-        console.log('Request.query.token: ' + request.query.token);
-        console.log('Request.query.user_name: ' + request.query.user_name);
-    } else {
-        console.log('Request.query: null');
-    }
-
-
-    var reply = slack.respond(request.body,function(hook) {
-
-        return {
-            text: 'Booyah, ' + hook.user_name,
-            username: 'Superfly'
-        };
+    var reply = slack_in.respond(request.body,function(hook) {
+        if (hook.user_name != 'slackbot'){
+            return {
+                text: 'Booyah, ' + hook.user_name,
+                username: 'Superfly'
+            };
+        }
 
     });
-
-    console.log('Response.body: '+response.body);
 
     response.json(reply);
 
 });
 
+//light it up
 app.listen(app.get('port'), function() {
   console.log("Node app is running at port:" + app.get('port'))
 });
