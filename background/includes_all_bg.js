cdf641dd8c242ad6e831262aba0cb267aa3fdb57
joereger <joe@joereger.com>
2014-10-17 10:21:39 -0400

    removing one task

--- background/includes_all_bg.js
+++ background/includes_all_bg.js
@@ -2,4 +2,4 @@
 exports.number_of_slacks = require('./number_of_slacks.js');
 exports.top_slackers = require('./top_slackers.js');
 exports.top_words = require('./top_words.js');
-exports.sentiment_good = require('./sentiment_good.js');
+exports.sentiment_report = require('./sentiment_report.js');
--- controllers/index.js
+++ controllers/index.js
@@ -11,32 +11,11 @@ router.get('/', function(req, res) {
         var start_date =  common.moment().startOf('week').toDate();
         var end_date = new Date();
 
-        all_bg_tasks.sentiment_good.run(start_date, end_date, 'since the start of the week');
-
-
-//        var associativeArray = new Array();
-//        associativeArray['city'] = 8;
-//        associativeArray['state'] = 3;
-//        associativeArray['country'] = -1;
-//        console.log(associativeArray);
-//        associativeArray.sort(function(a, b) {
-//            a = a[1];
-//            b = b[1];
-//            return a < b ? -1 : (a > b ? 1 : 0);
-//        });
-//        console.log(associativeArray);
-
-
-//        console.log('moment()='+common.moment().format());
-//        console.log('moment().startOfDay(\'\')='+common.moment().startOf('day').format());
-//        console.log('moment().startOf(\'day\').toDate()='+common.moment().startOf('day').toDate());
-//
-
+        all_bg_tasks.sentiment_report.run(start_date, end_date, 'since the start of the week');
 
 
         //secret response
         res.send("<div style='font-size: 78px;'>the superfly eats purple diamond croissants for brunch</div>");
-
 
 
     } else {
