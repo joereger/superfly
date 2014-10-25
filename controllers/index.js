eb21871cf58b2c962bb3fd00cca0b7826a84ca10
joereger <joe@joereger.com>
2014-10-24 20:44:32 -0400

    limiting sentiment report to friday

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,8 @@ router.get('/', function(req, res) {
     //hack to make local testing easier
     if (req.query.password == 'superfly'){
 
+
+
         //secret response
         res.send("<div style='font-size: 78px;'>the superfly eats purple diamond croissants for brunch</div>");
 
